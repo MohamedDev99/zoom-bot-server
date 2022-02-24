5763e6c909faa569134b91b2e527b4b64ac2006c
Mohamed EL Moumny <88399051+MohamedDev99@users.noreply.github.com>
2022-02-24 14:48:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // initialize Stream Chat SDK
 
 const polly = new AWS.Polly({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
     region: "us-east-1",
 });
 
